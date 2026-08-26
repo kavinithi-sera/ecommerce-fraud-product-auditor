ff6168018088ae7e1e74bbcc1fc0e813e4595859
ShwetaExplores <shwetayadav3593@gmail.com>
2026-08-26 21:57:42 +0530

    Update popup.js
    Update API URLs in popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
-const BACKEND_URL = "https://ecommerce-fraud-detection-ext.onrender.com";
-const FEEDBACK_URL = "https://ecommerce-fraud-detection-ext.onrender.com";
+const BACKEND_URL = "https://ecommerce-fraud-detection-ext.onrender.com/api/analyze";
+const FEEDBACK_URL = "https://ecommerce-fraud-detection-ext.onrender.com/api/feedback";
 
 let lastResult = null;
 let selectedVote = null;
